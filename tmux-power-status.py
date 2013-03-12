aceb96f1ccab6c5da88d3c61645d8981f6b6c375
Phil Brown <phil@bacon>
2013-03-12 11:09:44 -0700

    Refactor tmux-power-status

--- tmux-power-status.py
+++ tmux-power-status.py
@@ -1,10 +1,10 @@
-# This script takes a battery name argument, and prints a pattery status line
+# This script takes a battery name argument, and prints a battery gauge
 # for tmux status bar, which indicates charge level and time to (dis)charge
 
 import sys
 import os
 
-POWER_METER_LENGTH = 10
+GAUGE_LENGTH = 10
 
 if len(sys.argv) < 2:
     sys.exit("Usage: %s sys_battery_subdir" % sys.argv[0])
@@ -26,50 +26,60 @@ with open('status', 'r') as f:
 def tmux_color(color_name):
     return "#[fg=" + color_name + "]"
 
-def draw_power_meter(time_str, power_level, power_tick, power_tick_minor, power_meter_color):
-    major_ticks = POWER_METER_LENGTH * power_level
+def draw_gauge(time_str, power_level, power_tick, power_tick_minor, gauge_color):
+    major_ticks = GAUGE_LENGTH * power_level
     major_ticks_draw = int(major_ticks)
-    empty_ticks = POWER_METER_LENGTH - major_ticks_draw - 1
+    empty_ticks = GAUGE_LENGTH - major_ticks_draw - 1
 
     if major_ticks % 1 > 0.5:
         power_tick_minor = power_tick
 
-    power_meter = power_tick * major_ticks_draw
-    power_meter += power_tick_minor
-    power_meter += " " * empty_ticks
-    print "%s [%s%s%s]" % (time_str, power_meter_color, power_meter, tmux_color('default'))
+    gauge = power_tick * major_ticks_draw
+    gauge += power_tick_minor
+    gauge += " " * empty_ticks
+    print "%s [%s%s%s]" % (time_str, gauge_color, gauge, tmux_color('default'))
 
-# time string from positive float hours
-def get_time_str(hours_float):
-    hours_fpart = hours_float % 1
-    return "%d:%02d" % (int(hours_float), int(hours_fpart * 60))
+# Returns time string
+def time_to_empty(energy_remain, power_now):
+    if power_now != 0:
+        hours_remain = energy_remain / power_now
+        hours_fpart = hours_remain % 1
+        return "%d:%02d" % (int(hours_remain), int(hours_fpart * 60))
+    else:
+        return ''
 
 try:
     if power_status == "Full":
         on_ac = " AC "
-        padding = "=" * ((POWER_METER_LENGTH - len(on_ac))/ 2)
+        padding = "=" * ((GAUGE_LENGTH - len(on_ac))/ 2)
         print "[%s%s%s]" % (padding, on_ac, padding)
         sys.exit(0)
 
-    power_remain = energy_now / energy_full
+    charge_remain = energy_now / energy_full
 
     if power_status == "Charging":
-        hours_remain = (energy_full - energy_now) / power_now
-        draw_power_meter("-" + get_time_str(hours_remain), power_remain, ">", "-", tmux_color('blue'))
+        energy_remain = energy_full - energy_now
+        # Negative "discharge" time
+        time_str = time_to_empty(energy_full - energy_now, power_now)
+        if time_str != '':
+            time_str = '-' + time_str
+        draw_gauge(time_str, charge_remain, ">", "-", tmux_color('blue'))
+
     elif power_status == "Discharging":
-        hours_remain = energy_now / power_now
-        if power_remain < 0.1:
-            power_meter_color = 'magenta'
-        elif power_remain < 0.25:
-            power_meter_color = 'red'
-        elif power_remain < 0.5:
-            power_meter_color = 'yellow'
+        time_str = time_to_empty(energy_now, power_now)
+        if charge_remain < 0.1:
+            gauge_color = 'magenta'
+        elif charge_remain < 0.25:
+            gauge_color = 'red'
+        elif charge_remain < 0.5:
+            gauge_color = 'yellow'
         else:
-            power_meter_color = 'green'
-        draw_power_meter(get_time_str(hours_remain), power_remain, "=", "-", tmux_color(power_meter_color))
+            gauge_color = 'green'
+        draw_gauge(time_str, charge_remain, "=", "-", tmux_color(gauge_color))
+
     else:
         print "[Unknown Power State]"
 
 except Exception as e:
-    print "xx:xx [%s]" % ("x" * POWER_METER_LENGTH)
+    print "xx:xx [%s]" % ("x" * GAUGE_LENGTH)
     print e
