abdfe3f67d705feaf40b63f8b199e1cb4e6f151a
Phil Brown <pbrown@palantir.com>
2013-03-19 13:37:24 -0700

    Redo RAM totals: used% (used+cache%)

--- ram-cpu-monitor.sh
+++ ram-cpu-monitor.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-loadavg=$(cat /proc/loadavg | awk '{print $1, $2, $3}')
-ram=$(free | sed -n '2 p' | awk '{printf("%.0f%% (%.0f%%)", ($3-$6)/$2*100, $7/$2*100)}')
+loadavg=$(awk '{print $1, $2, $3}' /proc/loadavg)
+ram=$(free | awk '
+NR == 2 {total=$2; used_with_cache=$3}
+NR == 3 {used=$3}
+END {printf("%.0f%% (%.0f%%)", used/total*100, used_with_cache/total*100)}')
 echo "$ram  $loadavg"
